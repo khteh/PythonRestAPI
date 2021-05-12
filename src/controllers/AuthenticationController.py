e25e4cd4f9c615108437bb5a97bc819b016dcba5
Teh Kok How <funcoolgeek@gmail.com>
2021-05-12 11:49:50 +0800

    Add oidc login/out

--- src/controllers/AuthenticationController.py
+++ src/controllers/AuthenticationController.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from ..models.UserModel import UserModel, UserSchema
 from ..common.Authentication import Authentication
 from ..common.Response import custom_response
+from ..common.Authentication import oidc
 auth_api = Blueprint("auth", __name__)
 user_schema = UserSchema()
 @auth_api.context_processor
@@ -46,6 +47,39 @@ def login():
             return render_template("login.html", title="Welcom to Python Flask RESTful API", error=errors)
     return render_template("login.html", title="Welcom to Python Flask RESTful API")
 
+@auth_api.route("/login_oidc", methods=["GET", "POST"])
+@oidc.require_login
+def login_oidc():
+    """Example for protected endpoint that extracts private information from the OpenID Connect id_token.
+       Uses the accompanied access_token to access a backend service.
+    """
+    if oidc.user_loggedin:
+        try:
+            info = oidc.user_getinfo(['preferred_username', 'email', 'sub'])
+            username = info.get('preferred_username')
+            email = info.get('email')
+            user_id = info.get('sub')
+            if user_id in oidc.credentials_store:
+                try:
+                    from oauth2client.client import OAuth2Credentials
+                    access_token = OAuth2Credentials.from_json(oidc.credentials_store[user_id]).access_token
+                    headers = {'Authorization': 'Bearer %s' % (access_token)}
+                    session["user"] = {"id": user_id, "username": username, "email": email, "token": access_token}
+                    # YOLO
+                    #greeting = requests.get('http://localhost:8080/greeting', headers=headers).text
+                    #return render_template("index.html")
+                    print(f"[Auth] UserId: {user_id}, UserName: {username}, Email: {email} logged in")
+                    return redirect(session["url"] if "url" in session else url_for("home.index"))
+                except Exception as e:
+                    print(f"Login fails! {str(e)}")
+                    return render_template("login_error.html", error_message="Invalid credentials!")
+            else:
+                print(f"Invalid credentials!")
+                return render_template("login_error.html", error_message="Invalid credentials!")
+        except Exception as e:
+            print(f"Please login to continue. {str(e)}")
+        return render_template("login_error.html", error_message="Invalid credentials!")
+
 @auth_api.route("/logout")
 @Authentication.auth_required
 def logout():
@@ -57,6 +91,16 @@ def logout():
     session['logged_in'] = False
     session["token"] = ""
     return render_template("login.html", title="Welcom to Python Flask RESTful API")
+
+@auth_api.route('/logout_oidc')
+@oidc.require_login
+def logout_oidc():
+    """Performs local logout by removing the session cookie."""
+    if oidc.user_loggedin and session["user"]:
+        print(f"[Auth] UserId: {session['user']['id']}, UserName: {session['user']['username']}, Email: {session['user']['email']} logged out")
+        oidc.logout()
+        session["user"] = None
+    return redirect(url_for("home.index"))
 
 @auth_api.route("/profile")
 @Authentication.auth_required
