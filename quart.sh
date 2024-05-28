cb1bc24d051e05400bc7edb04922225310bcbed5
Kok How, Teh <funcoolgeek@gmail.com>
2024-05-28 17:56:52 +0800

    (1) Upgrade packages (2) Fix quart/hypercorn config

--- quart.sh
+++ quart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 openssl req -new -newkey rsa:4096 -x509 -nodes -days 365 -keyout server.key -out server.crt -subj "/C=SG/ST=Singapore/L=Singapore /O=Kok How Pte. Ltd./OU=PythonFlaskRestAPI/CN=localhost/emailAddress=funcoolgeek@gmail.com" -passin pass:PythonFlaskRestAPI
-pipenv run hypercorn --config=hypercorn.toml --reload --quic-bind 0.0.0.0:4433 --certfile server.crt --keyfile server.key --bind 0.0.0.0:8080 src.main:app
+#pipenv run hypercorn --config=hypercorn.toml --reload --quic-bind 0.0.0.0:4433 --certfile server.crt --keyfile server.key --bind 0.0.0.0:8080 src.main:app
+pipenv run hypercorn --config=hypercorn.toml --reload src.main:app
