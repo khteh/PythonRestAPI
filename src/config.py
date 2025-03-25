ebf117612fbe89f8c47ffb367ad6f0a2aa732a6f
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-25 15:24:15 +0800

    Code cleanup

--- src/config.py
+++ src/config.py
@@ -2,27 +2,6 @@ import os, json
 from dotenv import load_dotenv
 from urllib import parse
 load_dotenv()
-"""
-class Development(object):
-    DEBUG = True
-    TESTING = False
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('POSTGRESQL_USER')}:{os.environ.get('POSTGRESQL_PASSWORD')}@svc-postgresql/library"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    OIDC_CLIENT_SECRETS = "manifests/oidc-secret.json"
-
-class Production(object):
-    DEBUG = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('POSTGRESQL_USER')}:{os.environ.get('POSTGRESQL_PASSWORD')}@svc-postgresql/library"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    OIDC_CLIENT_SECRETS = "manifests/oidc-secret.json"
-
-app_config = {
-    "development": Development,
-    "production": Production,
-}
-"""
 class Config:
     DEBUG = False
     TESTING = False
