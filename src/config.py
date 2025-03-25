ea4577fe8a81fb150fb498e5c40b36526683875b
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-25 15:23:15 +0800

    Define Config class

--- src/config.py
+++ src/config.py
@@ -1,11 +1,9 @@
-import os
+import os, json
 from dotenv import load_dotenv
+from urllib import parse
 load_dotenv()
-
+"""
 class Development(object):
-    """
-    Development environment configuration
-    """
     DEBUG = True
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
@@ -14,9 +12,6 @@ class Development(object):
     OIDC_CLIENT_SECRETS = "manifests/oidc-secret.json"
 
 class Production(object):
-    """
-    Production environment configuration
-    """
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('POSTGRESQL_USER')}:{os.environ.get('POSTGRESQL_PASSWORD')}@svc-postgresql/library"
@@ -27,3 +22,14 @@ app_config = {
     "development": Development,
     "production": Production,
 }
+"""
+class Config:
+    DEBUG = False
+    TESTING = False
+    with open('/etc/pythonrestapi_config.json', 'r') as f:
+        config = json.load(f)
+    SECRET_KEY = config["SECRET_KEY"] or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{config['DB_HOST']}/library"
+    POSTGRESQL_DATABASE_URI = f"postgresql://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{config['DB_HOST']}/library"
+    JWT_SECRET_KEY = config["JWT_SECRET_KEY"]
+    OIDC_CLIENT_SECRETS = config["OIDC_CLIENT_SECRETS"]
--- src/main.py
+++ src/main.py
@@ -27,9 +27,8 @@ def create_app() -> Quart:
     # App initialization
     app = Quart(__name__, template_folder='view/templates', static_url_path='', static_folder='view/static')
     app.config.from_file("/etc/pythonrestapi_config.json", json.load)
-    if "SQLALCHEMY_DATABASE_URI" not in app.config:
-        app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{app.config['DB_HOST']}/library"
-        app.config["POSTGRESQL_DATABASE_URI"] = f"postgresql://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{app.config['DB_HOST']}/library"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{app.config['DB_HOST']}/library"
+    app.config["POSTGRESQL_DATABASE_URI"] = f"postgresql://{os.environ.get('DB_USERNAME')}:{parse.quote(os.environ.get('DB_PASSWORD'))}@{app.config['DB_HOST']}/library"
     app.register_blueprint(home_blueprint, url_prefix="/")
     app.register_blueprint(fibonacci_blueprint, url_prefix="/fibonacci")
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
