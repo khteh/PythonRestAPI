941390db715f41194df6ed0ad752496fab61ec2b
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-27 13:38:34 +0800

    Use postgresql+psycopg for connection string

--- src/models/Database.py
+++ src/models/Database.py
@@ -5,7 +5,7 @@ from quart_sqlalchemy import SQLAlchemyConfig
 from quart_sqlalchemy.framework import QuartSQLAlchemy
 with open('/etc/pythonrestapi_config.json', 'r') as f:
     config = json.load(f)
-connection_string = f"postgresql://{os.environ.get('DB_USERNAME')}:{parse.quote_plus(os.environ.get('DB_PASSWORD'))}@{config['DB_HOST']}/library"
+connection_string = f"postgresql+psycopg://{os.environ.get('DB_USERNAME')}:{parse.quote_plus(os.environ.get('DB_PASSWORD'))}@{config['DB_HOST']}/library"
 db = QuartSQLAlchemy(
   config = SQLAlchemyConfig(
       binds = dict(
