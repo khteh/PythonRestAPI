6fcf0325616406981ca273a9be18ea5b271fbcb2
Kok How, Teh <funcoolgeek@gmail.com>
2025-03-28 17:37:35 +0800

    Read credentials from CircleCI env

--- src/models/Database.py
+++ src/models/Database.py
@@ -1,10 +1,11 @@
-import os, json
+import os, json, sqlalchemy
 from urllib import parse
-import sqlalchemy
+from dotenv import load_dotenv
 from quart_sqlalchemy import SQLAlchemyConfig
 from quart_sqlalchemy.framework import QuartSQLAlchemy
+load_dotenv()
 if "Testing" in os.environ:
-    connection_string = f"postgresql+psycopg://{os.environ.get('DB_USERNAME')}:{parse.quote_plus(os.environ.get('DB_PASSWORD'))}@localhost:5432/library"
+    connection_string = f"postgresql+psycopg://{os.environ.get('POSTGRESQL_USER')}:{parse.quote_plus(os.environ.get('POSTGRESQL_PASSWORD'))}@localhost:5432/library"
 else:
     with open('/etc/pythonrestapi_config.json', 'r') as f:
         config = json.load(f)
