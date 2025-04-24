d4699ce66370adb8922785cae4da9bed1db5e3cc
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-24 12:41:15 +0800

    Log to both stdout and file in k8s cluster

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-import os, json, logging
+import os, json, logging, sys
 from dotenv import load_dotenv
 from urllib import parse
 load_dotenv()
@@ -32,6 +32,18 @@ class Config(metaclass=ConfigSingleton):
         The level parameter now accepts a string representation of the level such as ‘INFO’ as an alternative to the integer constants such as INFO.
         """
         logging.getLogger("httpx").setLevel(logging.WARNING)
-        logging.basicConfig(filename='/var/log/pythonrestapi/log', filemode='w', format='%(asctime)s %(levelname)-8s %(message)s', level=self.LOGLEVEL, datefmt='%Y-%m-%d %H:%M:%S')	
+        """
+        https://realpython.com/python-modulo-string-formatting/#fine-tune-your-output-with-conversion-flags
+        -	Justification of values that are shorter than the specified field width
+        The Hyphen-Minus Flag (-)
+        When a formatted value is shorter than the specified field width, it’s usually right-justified in the field. The hyphen-minus (-) flag causes the value to be left-justified in the specified field instead.
+        """
+        if config["ENVIRONMENT"] == "production":
+            logging.basicConfig(handlers=[
+                logging.FileHandler(filename='/var/log/pythonrestapi/log', mode='w'),
+                logging.StreamHandler(sys.stdout)
+            ], format='%(asctime)s %(levelname)-8s %(message)s', level=self.LOGLEVEL, datefmt='%Y-%m-%d %H:%M:%S')
+        else:
+            logging.basicConfig(filename='/var/log/pythonrestapi/log', filemode='w', format='%(asctime)s %(levelname)-8s %(message)s', level=self.LOGLEVEL, datefmt='%Y-%m-%d %H:%M:%S')
 
 config = Config()
