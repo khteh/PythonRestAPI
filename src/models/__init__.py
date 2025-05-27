7b0618bb69666155b3005370a6f34fd73e41ed72
Kok How, Teh <funcoolgeek@gmail.com>
2025-05-27 12:20:28 +0800

    Fix bycrypt import from Quart instead of Flask

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,5 @@
-import os
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from quart_bcrypt import Bcrypt
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 from .AuthorModel import AuthorModel, AuthorSchema
